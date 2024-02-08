07e6fd334cd817d869227fecb580f42a9149c0d4
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2024-02-08 18:08:38 -0300

    first commit

---
+++ semana01/aula01/meu_primeiro_codigo.js
@@ -0,0 +1,6 @@
+// importando e executando a biblioteca prompt
+const prompt = require("prompt-sync")();
+
+var nome = prompt("Digite seu nome: ") // Bruno Costa
+
+console.log('Seja bem-vindo ' + nome)
---
+++ semana01/aula01/tipos_de_dados.js
@@ -0,0 +1,22 @@
+// strings - textos
+var nome = "Bruno Costa"
+
+// number
+var ano = 2024
+
+// boolean - binário
+// false - 0
+var aprovado = false // true - 1
+
+// null
+var exemploDeTipo = null
+
+// undefined
+var exemploDeUndefined = undefined
+var idade; // undefined
+
+// object
+var aluno = {
+  nome: "Bruno",
+  turma: "Trip"
+}
